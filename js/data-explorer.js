2ca7f338dac07e7b0dec940c0678fd4590000420
slifty <slifty@gmail.com>
2013-02-03 23:38:09 -0500

    added rough draft of votes timeline

--- js/data-explorer.js
+++ js/data-explorer.js
@@ -3,8 +3,8 @@ $(function() {
 	var $base = $("#data-explorer");
 	var datasets = {};
 	var current_dataset = "";
+	var current_filters = {};
 
-	$(window).resize(function() { render_dataset(current_dataset); });
 	$("#data-explorer-graph").html("<img class='loader' src='img/ajax-loader.gif' />");
 
 	$.when(
@@ -13,6 +13,11 @@ $(function() {
 			method: "GET",
 			url: "data/ad_claims.json"
 		}).success(function(data) { datasets["ad_claims"] = data; }),
+		$.ajax({
+			dataType: "json",
+			method: "GET",
+			url: "data/voting_data.json"
+		}).success(function(data) { datasets["voting_data"] = data; }),
 		$.ajax({
 			dataType: "json",
 			method: "GET",
@@ -47,46 +52,104 @@ $(function() {
 	.then(function() {
 		var $option_activity = $("#data-option-activity")
 			.click(function() {
-				render_dataset(datasets["sessions_activity"]);
+				set_active_button($(this));
+				render_graph(datasets["sessions_activity"]);
 			});
 
 		var $option_city = $("#data-option-city")
 			.click(function() {
-				render_dataset(datasets["sessions_city"]);
+				set_active_button($(this));
+				render_graph(datasets["sessions_city"]);
 			});
 
 		var $option_city = $("#data-option-country")
 			.click(function() {
-				render_dataset(datasets["sessions_country"]);
+				set_active_button($(this));
+				render_graph(datasets["sessions_country"]);
 			});
 
 		var $option_city = $("#data-option-date")
 			.click(function() {
-				render_dataset(datasets["sessions_date"]);
+				set_active_button($(this));
+				render_graph(datasets["sessions_date"]);
 			});
 
 		var $option_city = $("#data-option-state")
 			.click(function() {
-				render_dataset(datasets["sessions_state"]);
+				set_active_button($(this));
+				render_graph(datasets["sessions_state"]);
 			});
 
 		var $option_city = $("#data-option-time")
 			.click(function() {
-				render_dataset(datasets["sessions_time"]);
+				set_active_button($(this));
+				render_graph(datasets["sessions_time"]);
 			});
 
-		render_dataset(datasets["sessions_activity"]);
+		var $filter_love = $("#data-filter-love")
+			.click(function() {
+				set_active_button($(this));
+				render_timeline("love");
+			});
+
+		var $filter_fail = $("#data-filter-fail")
+			.click(function() {
+				set_active_button($(this));
+				render_timeline("fail");
+			});
+
+		var $filter_fishy = $("#data-filter-fishy")
+			.click(function() {
+				set_active_button($(this));
+				render_timeline("fishy");
+			});
+
+		var $filter_false = $("#data-filter-fair")
+			.click(function() {
+				set_active_button($(this));
+				render_timeline("fair");
+			});
+
+
+		set_active_button($option_activity);
+		render_graph(datasets["sessions_activity"]);
 	})
 
-	function render_dataset(dataset) {
-		current_dataset = dataset;
+	function set_active_button($button) {
+			$(".active").removeClass("active");
+			$button.addClass("active");
+	}
+	function render_timeline(tag) {
+		var core_dataset = datasets["voting_data"];
+		var filtered_dataset = [];
+		var calculated_dataset = {};
+		var dataset = [];
+		for(var x in core_dataset) {
+			if(core_dataset[x]["tag"] != tag)
+				continue;
+			filtered_dataset.push(core_dataset[x]);
+		}
+		
+		for(var x in filtered_dataset) {
+			if(!(filtered_dataset[x].date_only in calculated_dataset)) {
+				calculated_dataset[filtered_dataset[x].date_only] = {
+					label: filtered_dataset[x].date_only,
+					value: 0
+				}
+			}
+			calculated_dataset[filtered_dataset[x].date_only].value++;
+		}
+		for(var x in calculated_dataset)
+			dataset.push(calculated_dataset[x]);
+		dataset = dataset.reverse();
+
 		var $graph = $("#data-explorer-graph")
 			.empty()
 			.append('<div id="data-explorer-yaxis" />')
-			.append('<div class="scroll"><div id="data-explorer-content" /><div id="data-explorer-xaxis" /></div>');
+			.append('<div class="scroll"><div id="data-explorer-content" /><div id="data-explorer-xaxis" /></div>')
+			.append('<div id="data-filters" />')
 		var $content = $("#data-explorer-content");
 
-		console.log($content.height("height"));
 		var w = dataset.length * 50;
 		var h = $content.height();
 		var padding = 5;
@@ -95,8 +158,8 @@ $(function() {
 			.domain([0, dataset.length])
 			.range([0, w]);
 		var yScale = d3.scale.linear()
-			.domain([0, d3.max(dataset, function(d) { return parseInt(d.value); })])
-			.range([h - padding, 0]);
+			.domain([0, 2500])
+			.range([h - padding, 10]);
 		var yAxis = d3.svg.axis()
 			.scale(yScale)
 			.orient("left")
@@ -112,7 +175,72 @@ $(function() {
 			.attr("height", h)
 		var svg_xaxis = d3.select("#data-explorer-xaxis")
 			.append("svg")
+			.attr("width", w + 40)
+
+		svg_yaxis.append("g")
+			.call(yAxis)
+    		.attr("transform", "translate(60,0)")
+			.attr("class","axis");
+		var bars = svg_content.selectAll("rect")
+			.data(dataset);
+		var labels = svg_xaxis.selectAll("text")
+			.data(dataset);
+
+		bars.enter()
+			.append("rect")
+			.attr("x", function(d, i) { return xScale(i); })
+			.attr("width", function(d) { return w / dataset.length - 2; })
+			.attr("y", function(d) { return h - padding; })
+			.transition()
+			.duration(300)
+			.delay(function(d,i) { return i * 50; })
+			.attr("height", function(d) { return h - padding - yScale(d.value); })
+			.attr("y", function(d) { return yScale(d.value); })
+
+		labels.enter()
+			.append("text")
+			.attr("x", function(d, i) {
+            	return xScale(i) + 15;
+			})
+			.attr("y", 10)
+			.text(function(d) { return d.label })
+    		.attr("transform", function(d, i) { return "rotate(40," + (xScale(i) + 15) + ",10)"; })
+	}
+
+	function render_graph(dataset) {
+		var current_dataset = dataset;
+		var $graph = $("#data-explorer-graph")
+			.empty()
+			.append('<div id="data-explorer-yaxis" />')
+			.append('<div class="scroll"><div id="data-explorer-content" /><div id="data-explorer-xaxis" /></div>');
+		var $content = $("#data-explorer-content");
+
+		var w = dataset.length * 50;
+		var h = $content.height();
+		var padding = 5;
+
+		var xScale = d3.scale.linear()
+			.domain([0, dataset.length])
+			.range([0, w]);
+		var yScale = d3.scale.linear()
+			.domain([0, d3.max(dataset, function(d) { return parseInt(d.value); })])
+			.range([h - padding, 10]);
+		var yAxis = d3.svg.axis()
+			.scale(yScale)
+			.orient("left")
+			.ticks(5);
+
+		var svg_content = d3.select("#data-explorer-content")
+			.append("svg")
 			.attr("width", w)
+			.attr("height", h)
+		var svg_yaxis = d3.select("#data-explorer-yaxis")
+			.append("svg")
+			.attr("width", 100)
+			.attr("height", h)
+		var svg_xaxis = d3.select("#data-explorer-xaxis")
+			.append("svg")
+			.attr("width", w + 40)
 
 		svg_yaxis.append("g")
 			.call(yAxis)
